9222bb807ae73b94d00fa01a7649a9a77b691807
Veitzn1 <158764398+Veitzn1@users.noreply.github.com>
2024-06-17 16:02:44 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.s": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.veitzn.site": dockerHub,
+  "quay.veitzn.site": "https://quay.io",
+  "gcr.veitzn.site": "https://gcr.io",
+  "k8s-gcr.veitzn.site": "https://k8s.gcr.io",
+  "k8s.veitzn.site": "https://registry.k8s.io",
+  "ghcr.veitzn.site": "https://ghcr.io",
+  "cloudsmith.veitzn.site": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
